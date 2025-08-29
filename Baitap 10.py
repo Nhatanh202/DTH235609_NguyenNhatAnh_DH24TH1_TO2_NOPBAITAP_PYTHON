be584ad426d534e7cbf873044d063b87d9575515
Orbexis <nnhatanh487@gmail.com>
2025-08-29 09:02:30 +0700

    Nop bai

--- Baitap 10.py
+++ Baitap 10.py
@@ -1,14 +1,6 @@
-# Function to draw a Christmas tree with a given height
-def draw_tree(height):
-  # Loop through each row of the tree
-  for i in range(1, height + 1):
-    # Print the spaces before the asterisks on each row
-    for j in range(height - i):
-      print(" ", end="")
-    # Print the asterisks on each row
-    for j in range(2 * i - 1):
-      print("*", end="")
-    # Move to the next line
-    print()
-# Call the function to draw a tree with a height of 5
-draw_tree(8)
+sao = [1, 3, 5, 3, 5, 7, 2, 2]
+for i in sao:
+    if i == 2:
+        print(' ' * (max(sao) - i) + '*  ' * i)
+    else:
+        print(' ' * (max(sao) - i) + '* ' * i)
