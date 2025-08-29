5321950c4a90fffb60f142b33c56c9ff8467999a
Orbexis <nnhatanh487@gmail.com>
2025-08-29 10:58:21 +0700

    Nop bai

--- Baitap chuong_2/Cau1.py
+++
@@ -1,9 +0,0 @@
-import math
-try :
-    r = float(input("Nhap ban kinh hinh tron: "))
-    cv = 2 * math.pi * r
-    dt = r ** 2
-    print("Chu vi = ", cv)
-    print("Dien tich = ", dt)
-except :
-    print("Lỗi rồi!")
